0153ca76c5243703fbb9bf17a4f0b8297f11dc09
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2015-09-11 11:36:38 +0300

    Logic added

--- HelloWorld.php
+++ HelloWorld.php
@@ -6,3 +6,19 @@
 	echo $first_name." ".$last_name;
 	
 ?>
+<br>
+<?php
+
+	$age = 24;
+	// kui on väiksem kui 18, siis on alaealine
+	// kui on suurem, siis täisealine
+	if($age < 18) {
+	//tõene
+	echo "alaealine";	
+	
+	} else {
+	// väär
+	echo "täisealine";
+	}
+
+?>
